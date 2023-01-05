e8c9df18cdd00379bd1ead54814f23c709745c94
Andrew Depke <andrewdepke@gmail.com>
2023-01-04 23:50:09 -0700

    Fixing build script for raspberry pi zero arch

--- scripts/buildFFmpeg.sh
+++ scripts/buildFFmpeg.sh
@@ -11,12 +11,17 @@ then
     exit 0
 fi
 
+# Decoder/encoder options:
+# Decoding: libx264 (SW), MMAL (HW)
+# Encoding: OpenMAX (HW)
+# See: https://trac.ffmpeg.org/wiki/HWAccelIntro
+
 mkdir -p $installDir/sources $installdir/bin && \
 chmod a+rw build && \
 cd $installDir/sources && \
 curl https://www.ffmpeg.org/releases/ffmpeg-5.1.2.tar.bz2 --output ffmpeg.tar.bz2 && \
 tar xjf ffmpeg.tar.bz2 && \
-cd ffmpeg && \
+cd ffmpeg-5.1.2 && \
 PATH="$installDir/bin:$PATH" PKG_CONFIG_PATH="$installDir/build/lib/pkgconfig" ./configure \
   --prefix="$installDir/build" \
   --pkg-config-flags="--static" \
@@ -25,11 +30,19 @@ PATH="$installDir/bin:$PATH" PKG_CONFIG_PATH="$installDir/build/lib/pkgconfig" .
   --extra-libs="-lpthread -lm" \
   --ld="g++" \
   --bindir="$installDir/bin" \
+  --arch=arm \
+  --cpu=arm1176jzf-s \
+  --target-os=linux \
   --enable-gpl \
+  --enable-nonfree \
+  --disable-doc \
   --enable-libx264 \
-  --disable-hwaccels \
-  --disable-vaapi \
-  --disable-vdpau && \
+  --enable-omx \
+  --enable-omx-rpi \
+  --enable-mmal \
+  --enable-neon \
+  --enable-rpi \
+  --enable-hardcoded-tables && \
 PATH="$installDir/bin:$PATH" make -j$numCores && \
 make -j$numCores install && \
 echo "$libDir" | tee -a /etc/ld.so.conf.d/ffmpeg.conf && \
