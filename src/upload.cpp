b73245bdcaee00a5fac4e08f3482f9b886f6e325
Andrew Depke <andrewdepke@gmail.com>
2023-12-21 20:19:06 -0700

    Stdout parsing

--- src/upload.cpp
+++ src/upload.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <filesystem>
 #include <set>
+#include <cstdio>
 
 int uploadMedia() {
     // Find all clips and upload them to the remote storage.
@@ -16,6 +17,18 @@ int uploadMedia() {
 
         const auto convertCommand = "./python/convert.py --file " + entry.path().string();
         auto returnCode = system(convertCommand.c_str());
+
+        std::array<char, 128> convertBuffer;
+        std::string convertStdout;
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(convertCommand.c_str(), "r"), pclose);
+        if (!pipe) {
+            std::cerr << "Failed to launch conversion subprocess!\n";
+            continue;
+        }
+
+        while (fgets(convertBuffer.data(), convertBuffer.size(), pipe.get()) != nullptr) {
+            convertStdout += convertBuffer.data();
+        }
 
         if (returnCode != 0) {
             std::cerr << "Failed to convert! Code: " << returnCode << "\n";
@@ -30,7 +43,7 @@ int uploadMedia() {
 
         std::cout << "Uploading...\n";
 
-        auto convertedPath = entry.path();
+        auto convertedPath = std::filesystem::path(convertStdout);
         convertedPath.replace_extension(".mp4");
         const auto uploadCommand = "./python/upload.py --file " + convertedPath.string();
         returnCode = system(uploadCommand.c_str());
