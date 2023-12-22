8bd08a099f77c91d2b650f2ad4fa4864718bdb3d
Andrew Depke <andrewdepke@gmail.com>
2023-12-21 17:27:35 -0700

    Fix path

--- scripts/install.bash
+++ scripts/install.bash
@@ -9,7 +9,7 @@ sudo apt install -y g++ libx264-dev xz-utils openssl libssl-dev python3 python3-
 ./premake5 gmake2
 make
 
-./setupPython.bash
+./scripts/setupPython.bash
 
 sudo cp utils/dashcam.service.template /etc/systemd/system/dashcam.service
 echo "User=$(whoami)" | sudo tee -a /etc/systemd/system/dashcam.service
