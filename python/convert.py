80b2962309d187a86509a29467440bbe7395cff6
Andrew Depke <andrewdepke@gmail.com>
2023-12-21 20:25:50 -0700

    Uploaded python changes

--- python/convert.py
+++ python/convert.py
@@ -2,20 +2,29 @@
 
 import argparse
 import os
+import sys
 import subprocess
 
 def main():
     parser = argparse.ArgumentParser(description="Tool to covert video files to mp4")
-    parser.add_argument("--file", dest="file", required=True)
+    parser.add_argument("-f", "--file", type=str, required=True)
+    parser.add_argument("-d", "--destination", type=str, required=False)
     parsedArgs = parser.parse_args()
 
     dest = os.path.splitext(os.path.basename(parsedArgs.file))[0] + ".mp4"
+    if parsedArgs.destination is not None:
+        dest = os.path.join(parsedArgs.destination, dest)
+
     ffmpegCmd = f"ffmpeg -y -hide_banner -loglevel error -r 30 -i {parsedArgs.file} -c:a copy {dest}"
 
     cmd = subprocess.Popen(ffmpegCmd.split(" "), stderr=subprocess.PIPE)
     _, stderr = cmd.communicate()
 
-    print(stderr.decode(), end="")
+    print(stderr.decode(), end="", file=sys.stderr)
+
+    # Prints the filename output on success.
+    if cmd.returncode == 0:
+        print(dest)
 
     return cmd.returncode
 
