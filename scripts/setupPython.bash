0a356fce86f5e9743d4e805d81e30751d87e9713
Andrew Depke <andrewdepke@gmail.com>
2024-01-21 15:34:13 -0700

    GPIO library

--- scripts/setupPython.bash
+++ scripts/setupPython.bash
@@ -5,4 +5,4 @@ set -e
 curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
 trap "rm ./get-pip.py" EXIT
 python3 ./get-pip.py
-python3 -m pip install google-auth google-api-python-client PyDrive2
+python3 -m pip install google-auth google-api-python-client PyDrive2 RPi.GPIO
